7b170ee90acb19551fcb3b7cca56d16c674c413f
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-04-24 00:08:48 +0700

    added navigation-layer

---
+++ FitoNyash/FitoNyash/NavigationLayer/Coordinators/Coordinator.swift
@@ -0,0 +1,42 @@
+//
+//  Coordinator.swift
+//  FitoNyash
+//
+//  Created by Никита Кочанов on 23.04.2025.
+//
+
+import UIKit
+
+enum CoordinatorType {
+    case app
+    case onboarding
+    case home
+    case order
+    case favorite
+    case profile
+}
+
+protocol CoordinatorProtocol: AnyObject {
+    var childCoordinators: [CoordinatorProtocol] { get set }
+    var type: CoordinatorType { get }
+    var navigationController: UINavigationController? { get set }
+    var finishDelegate: CoordinatorFinishDelegate? { get set }
+    
+    func start()
+    func finish()
+}
+
+extension CoordinatorProtocol {
+    func addChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators.append(childCoordinator)
+    }
+    func removeChildCoordinator(_ childCoordinator: CoordinatorProtocol) {
+        childCoordinators = childCoordinators.filter { $0 !== childCoordinator }
+    }
+}
+
+protocol CoordinatorFinishDelegate: AnyObject {
+    func coordinatorDidFinish(childCoordinator: CoordinatorProtocol)
+}
+
+
