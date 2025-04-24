c9b73dc709ad0f17c8f796ecdbdddfe7b38f8d61
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-04-24 20:33:53 +0700

    added custom FNButton view

---
+++ FitoNyash/FitoNyash/UILayer/Auth/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  FitoNyash
+//
+//  Created by Никита Кочанов on 24.04.2025.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ FitoNyash/FitoNyash/UILayer/Auth/Views/BottomView.swift
@@ -0,0 +1,12 @@
+//
+//  BottonView.swift
+//  FitoNyash
+//
+//  Created by Никита Кочанов on 24.04.2025.
+//
+
+import UIKit
+
+class BottomView: UIView {
+
+}
