a8cc78ac8dd1df06a15101c6fad2afabd7eccf9b
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-04-27 02:23:06 +0700

    added new factory method pattern for better work of coordinators

--- FitoNyash/FitoNyash/NavigationLayer/Coordinators/AppCoordinator.swift
+++ FitoNyash/FitoNyash/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -10,19 +10,20 @@ import UIKit
 class AppCoordinator: Coordinator{
     
     private let userStorage = UserStorage.shared
+    private let factory = SceneFactory.self
     
     override func start() {
-//        showOnboardingFlow()
+        showOnboardingFlow()
 //        if userStorage.passedOnboarding {
 //            showMainFlow()
 //        } else {
 //            showOnboardingFlow()
 //        }
         
-        let loginPresenter = LoginPresenter(coordinator: self)
-        
-        let loginVC = LoginViewController(viewOutput: loginPresenter, state: .base)
-        navigationController?.pushViewController(loginVC, animated: false)
+//        let loginPresenter = LoginPresenter(coordinator: self)
+//        
+//        let loginVC = LoginViewController(viewOutput: loginPresenter, state: .base)
+//        navigationController?.pushViewController(loginVC, animated: false)
         
     }
     
@@ -36,45 +37,11 @@ class AppCoordinator: Coordinator{
 private extension AppCoordinator {
     func showOnboardingFlow() {
         guard let navigationController = navigationController else { return }
-        let onboardingCoordinator = OnboardingCoordinator(type: .onboarding, navigationController: navigationController, finishDelegate: self)
-        addChildCoordinator(onboardingCoordinator)
-        onboardingCoordinator.start()
+        factory.makeOnboardingFlow(coordinator: self, finishDelegate: self, navigationController: navigationController)
     }
     func showMainFlow() {
         guard let navigationController = navigationController else { return }
-        
-        let homeNavigationController = UINavigationController()
-        let homeCoordinator = HomeCoordinator(type: .home, navigationController: homeNavigationController)
-        homeNavigationController.tabBarItem = UITabBarItem(title: "", image: UIImage(systemName: "house"), tag: 0)
-        homeCoordinator.finishDelegate = self
-        homeCoordinator.start()
-        
-        let orderNavigationController = UINavigationController()
-        let orderCoordinator = OrderCoordinator(type: .order, navigationController: orderNavigationController)
-        orderNavigationController.tabBarItem = UITabBarItem(title: "Order", image: UIImage(systemName: "house"), tag: 1)
-        orderCoordinator.finishDelegate = self
-        orderCoordinator.start()
-        
-        let favoriteNavigationController = UINavigationController()
-        let favoriteCoordinator = FavoriteCoordinator(type: .favorite, navigationController: favoriteNavigationController)
-        favoriteNavigationController.tabBarItem = UITabBarItem(title: "Fav", image: UIImage(systemName: "house"), tag: 2)
-        favoriteCoordinator.finishDelegate = self
-        favoriteCoordinator.start()
-        
-        let profileNavigationController = UINavigationController()
-        let profileCoordinator = ProfileCoordinator(type: .profile, navigationController: profileNavigationController)
-        profileNavigationController.tabBarItem = UITabBarItem(title: "profile", image: UIImage(systemName: "house"), tag: 3)
-        profileCoordinator.finishDelegate = self
-        profileCoordinator.start()
-        
-        addChildCoordinator(homeCoordinator)
-        addChildCoordinator(orderCoordinator)
-        addChildCoordinator(favoriteCoordinator)
-        addChildCoordinator(profileCoordinator)
-        
-        let tabBarControllers = [homeNavigationController, orderNavigationController, favoriteNavigationController, profileNavigationController]
-        let tabBarController = TabBarController(tabBarControllers: tabBarControllers)
-        
+        let tabBarController = factory.makeMainFlow(coordinator: self, finishDelegate: self)
         navigationController.pushViewController(tabBarController, animated: true)
     }
 }
